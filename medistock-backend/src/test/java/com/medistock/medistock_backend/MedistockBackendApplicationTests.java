57adfa1603029a6909e6d658313128e78513b7a6
jagruti_kad <jagrutikad612@gmail.com>
2026-07-30 23:08:12 +0530

    feat: backup setup

---
+++ medistock-backend/src/test/java/com/medistock/medistock_backend/MedistockBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medistock.medistock_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedistockBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
