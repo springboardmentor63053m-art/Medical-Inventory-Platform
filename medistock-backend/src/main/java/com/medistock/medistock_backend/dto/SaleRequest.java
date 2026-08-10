af1a99cfd4f07372bf4ff56fee197839c3fc130d
swastiksubhalaxmi <swastiksubhalaxmi78209@gmail.com>
2026-08-10 13:00:33 +0530

    fixing & improving new feature

--- medistock-backend/src/main/java/com/medistock/medistock_backend/dto/SaleRequest.java
+++ medistock-backend/src/main/java/com/medistock/medistock_backend/dto/SaleRequest.java
@@ -1,6 +1,7 @@
 package com.medistock.medistock_backend.dto;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -8,8 +9,12 @@ import java.util.List;
 
 @Data
 public class SaleRequest {
+    @NotBlank(message = "Customer name is required")
     private String customerName;
+
+    @NotBlank(message = "Customer phone is required")
     private String customerPhone;
+
     private String paymentMethod;
     private BigDecimal discountAmount;
 
